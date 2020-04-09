0f2d5b7458288f044d86288f56dca4acd3b989cd
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-04-09 08:10:41 +0100

    Edit some lines

--- src/estimator.js
+++ src/estimator.js
@@ -17,13 +17,9 @@ const covid19ImpactEstimator = (data) => {
 
   const exponent = Math.floor(numberOfDays / 3);
 
-  impact.infectionsByRequestedTime = Math.floor(
-    impact.currentlyInfected * 2 ** exponent
-  );
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** exponent;
 
-  severeImpact.infectionsByRequestedTime = Math.floor(
-    severeImpact.currentlyInfected * 2 ** exponent
-  );
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** exponent;
 
   impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
 
